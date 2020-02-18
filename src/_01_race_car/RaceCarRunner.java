7f1d27502fbd0d8c08679f552589b75c84d7ed9b
351054 <351054@0338S-Z-90TDE0K.sdcs.local>
2020-02-18 14:22:20 -0800

    2/18/20

--- src/_01_race_car/RaceCarRunner.java
+++ src/_01_race_car/RaceCarRunner.java
@@ -11,13 +11,16 @@ public class RaceCarRunner {
 		/* Do the following things without changing the RaceCar class */
 		
 		// 1. Create a RaceCar and place it in 5th position
-	
+	RaceCar lol = new RaceCar ("brand", 5);
 		// 2. Print the RaceCar's position in the race
-
+	int place = lol.getPositionInRace();
+System.out.println(place);
 		// 3. Crash the RaceCar
-		
+		lol.crash();
 		// 4. If the car is damaged. Bring it in for a pit stop.
-
+if(lol.isDamaged()) {
+	lol.pit();
+}
 		// 5. Help the car move into first place.
 
 	}
